b8002e9d72295243ca9020cd2afb943ec57fff43
unknown <lonelylok101@gmail.com>
2021-05-16 22:33:58 -0700

    add console log

--- test.ts
+++ test.ts
@@ -2,6 +2,7 @@ import {api} from './src/api';
 const cron = require('node-cron');
 
 cron.schedule(`*/10 * * * * *`, async () => {
-    console.log(new Date())
+    console.log(new Date(), 'start scan')
     await api().run();
+    console.log(new Date(), 'end scan')
   });
